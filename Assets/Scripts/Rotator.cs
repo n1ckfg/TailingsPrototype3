0da109ef57e36aa3abdca0d331afa7c4a4029a7f
Nick Fox-Gieg <nick@fox-gieg.com>
2021-03-10 20:19:43 -0500

    add emo cam group

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    public Vector3 rot = Vector3.zero;
+
+    private void Start() {
+        rot.x = 1f;
+    }
+
+    private void Update() {
+        transform.Rotate(rot);
+    }
+
+}
