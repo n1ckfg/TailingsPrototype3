e1716c3960b0087eefd38488db2ce42353c212eb
Nick Fox-Gieg <nick@fox-gieg.com>
2021-04-03 19:37:37 -0400

    materials

--- Assets/Scripts/Network/NetworkWsVideo.cs
+++ Assets/Scripts/Network/NetworkWsVideo.cs
@@ -6,18 +6,33 @@ public class NetworkWsVideo : MonoBehaviour {
 
     public NetworkManager netManager;
     public Renderer ren;
+    public float fps = 30f;
 
     private Texture2D tex;
-    private VideoMessage msg;
+    private float fpsInterval;
+	private bool blocked = false;
 
     private void Start() {
         tex = new Texture2D(1, 1);
+        fpsInterval = 1f / fps;
     }
 
-	public void UpdateData(VideoMessage _msg) {
-        tex.LoadImage(System.Convert.FromBase64String(_msg.video));
-        tex.Apply();
-        ren.sharedMaterial.mainTexture = tex;
+	private IEnumerator UpdateTexture(VideoMessage msg) {
+        if (msg.video.Length > 1) {
+            tex.LoadImage(System.Convert.FromBase64String(msg.video));
+            tex.Apply();
+            ren.sharedMaterial.mainTexture = tex;
+            ren.sharedMaterial.SetTexture("_EmissionMap", tex);
+        }
+        yield return new WaitForSeconds(fpsInterval);
+        blocked = false;
+	}
+
+	public void UpdateData(VideoMessage msg) {
+        if (!blocked) {
+            blocked = true;
+            StartCoroutine(UpdateTexture(msg));
+        }
     }
 
 }
