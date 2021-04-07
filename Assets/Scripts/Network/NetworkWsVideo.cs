fc6ea56151e503662100e4d82a3e686b6c0ae871
Nick Fox-Gieg <nick@fox-gieg.com>
2021-04-07 17:40:00 -0400

    test

--- Assets/Scripts/Network/NetworkWsVideo.cs
+++ Assets/Scripts/Network/NetworkWsVideo.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class NetworkWsVideo : MonoBehaviour {
 
     public NetworkManager netManager;
+    public RenderHeads.Media.AVProVideo.MediaPlayer video;
     public Renderer ren;
     public float fps = 30f;
+    public bool bypass = true;
 
     private Texture2D tex;
     private float fpsInterval;
@@ -15,6 +17,17 @@ public class NetworkWsVideo : MonoBehaviour {
     private void Start() {
         tex = new Texture2D(1, 1);
         fpsInterval = 1f / fps;
+    }
+
+    private void Update() {
+        if (Input.GetKeyUp(KeyCode.Tab)) {
+            bypass = !bypass;
+            if (bypass) {
+                video.Stop();
+            } else {
+                video.Play();
+            }
+        }
     }
 
 	private IEnumerator UpdateTexture(VideoMessage msg) {
@@ -29,7 +42,7 @@ public class NetworkWsVideo : MonoBehaviour {
 	}
 
 	public void UpdateData(VideoMessage msg) {
-        if (!blocked) {
+        if (!blocked && !bypass) {
             blocked = true;
             StartCoroutine(UpdateTexture(msg));
         }
